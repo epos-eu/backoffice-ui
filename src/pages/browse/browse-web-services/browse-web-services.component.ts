1ede53a4b53db72814064567f8cef74e6a174c4d
Janusz Czapski <janlav@bgs.ac.uk>
2022-08-11 15:37:32 +0100

    feat: tables for contact point, webservices

--- src/pages/browse/browse-web-services/browse-web-services.component.ts
+++ src/pages/browse/browse-web-services/browse-web-services.component.ts
@@ -1,26 +1,37 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { HelpersService } from 'src/services/helpers.service';
+import { SectionsService } from 'src/services/sections.service';
+import { SectionName } from 'src/utility/enums/sectionName.enum';
+import { SectionItem } from 'src/utility/objects/login/sectionItem';
+import { Sections } from 'src/utility/objects/login/sections';
 
 @Component({
   selector: 'app-browse-web-services',
   templateUrl: './browse-web-services.component.html',
   styleUrls: ['./browse-web-services.component.scss'],
 })
-export class BrowseWebServicesComponent {
-  public displayedColumns: string[] = ['uid', 'name', 'datePublished'];
-  // public dataSource!: MatTableDataSource<WebserviceDataSource>;
+export class BrowseWebServicesComponent implements OnInit {
+  public formatTimestamp = HelpersService.formatTimestamp;
+  public displayedColumns: string[] = [];
+  public dataSource!: MatTableDataSource<SectionItem>;
   public pageSizeOptions = [10, 25, 50, 100];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  // constructor() {}
+  constructor(private sectionsService: SectionsService) {}
 
-  // ngOnInit(): void {
+  public ngOnInit(): void {
+    this.sectionsService.sectionsObservable.subscribe((sections: Array<Sections>) => {
+      const webservice = sections.filter((item) => item.sectionName === SectionName.WEBSERVICE);
 
-  // }
-
-  // TODO: add type for row
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      this.displayedColumns = webservice[0].columns.map((item) => item.columnLabel);
+      this.dataSource = new MatTableDataSource(webservice[0].items);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
 }
