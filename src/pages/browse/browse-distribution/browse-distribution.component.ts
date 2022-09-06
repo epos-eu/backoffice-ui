ebc9201024f86214efa785283c7992ae1a780356
Daniel <danwar@bgs.ac.uk>
2022-09-06 14:22:47 +0100

    feat: refactored distribution table

--- src/pages/browse/browse-distribution/browse-distribution.component.ts
+++ src/pages/browse/browse-distribution/browse-distribution.component.ts
@@ -14,8 +14,8 @@ import { Sections } from 'src/utility/objects/login/sections';
   styleUrls: ['./browse-distribution.component.scss'],
 })
 export class BrowseDistributionComponent implements OnInit {
-  public displayedColumns: string[] = [];
-  public dataSource!: MatTableDataSource<SectionItem>;
+  public displayedColumns = ['uid', 'lastChange', 'status', 'comment', 'author'];
+  public dataSource!: MatTableDataSource<TableDetail>;
   public pageSizeOptions = [10, 25, 50, 100];
   public loading = false;
 
@@ -24,19 +24,46 @@ export class BrowseDistributionComponent implements OnInit {
 
   constructor(private router: Router, private sectionsService: SectionsService) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.loading = true;
     this.sectionsService.sectionsObservable.subscribe((sections: Array<Sections>) => {
-      const dataProducts = sections.filter((item) => item.sectionName === SectionName.DISTRIBUTION);
-      this.displayedColumns = dataProducts[0].columns.map((item) => item.columnLabel);
-      this.dataSource = new MatTableDataSource(dataProducts[0].items);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-      this.loading = false;
+      this.createTableObjects(sections);
     });
   }
 
-  // public rowClicked(row: SectionItem): void {
-  //   this.router.navigate(['/browse/distributions/details', row.instanceId]);
-  // }
+  public rowClicked(row: SectionItem): void {
+    this.router.navigate(['/browse/data-products/details', row.instanceId]);
+  }
+
+  private createTableObjects(sections: Array<Sections>) {
+    const tableDetails = new Array<TableDetail>();
+    const dataProducts = sections.filter((item) => item.sectionName === SectionName.DISTRIBUTION);
+    if (dataProducts[0]) {
+      dataProducts[0].items.forEach((item: SectionItem) => {
+        const detail: TableDetail = {
+          uid: item.cells[0].value,
+          lastChange: item.cells[1].value,
+          status: item.cells[2].value,
+          comment: item.cells[3].value,
+          author: item.cells[4].value,
+        };
+        tableDetails.push(detail);
+      });
+      this.initialiseTable(tableDetails);
+    }
+  }
+
+  private initialiseTable(details: Array<TableDetail>) {
+    this.dataSource = new MatTableDataSource(details);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.loading = false;
+  }
+}
+interface TableDetail {
+  uid: string;
+  lastChange: string;
+  status: string;
+  comment: string;
+  author: string;
 }
