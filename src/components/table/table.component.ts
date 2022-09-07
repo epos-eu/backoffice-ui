89e0eaf453c3be025ff25ff67933abc3f5c2eda0
Daniel <danwar@bgs.ac.uk>
2022-09-07 17:03:29 +0100

    feat: now iterating through distribution aray

--- src/components/table/table.component.ts
+++ src/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, Output, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
