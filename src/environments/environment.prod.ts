81ef9e07eb230a13a478c5f54a0965df8cbfd604
Janusz Czapski <janlav@bgs.ac.uk>
2023-04-12 11:29:24 +0100

    fix: proxy local API to deployed endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   server: '/epos-back-office/',
-  apiBaseUrl: 'https://ics-c.epos-ip.org/demo/k8s-epos-deploy/backoffice-test-env/api/v1/backoffice',
+  apiBaseUrl: 'http://localhost:4200/api',
   useLiveApi: true,
   apiMockUrl: 'http://localhost:4200/assets/data',
 };
