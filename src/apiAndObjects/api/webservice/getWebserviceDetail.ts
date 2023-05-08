fd484f9718a71c8117187ac642655c924097ed8e
valeriovinciarelli <Roma?=10>
2023-05-08 18:55:51 +0200

    update the build logic and deploy

--- src/apiAndObjects/api/webservice/getWebserviceDetail.ts
+++ src/apiAndObjects/api/webservice/getWebserviceDetail.ts
@@ -35,7 +35,7 @@ export class GetWebserviceDetail extends CacheableEndpoint<
       WebserviceDetailDataSource,
       new Promise((resolve) => {
         setTimeout(() => {
-          resolve(lastValueFrom(httpClient.get('/testpath/assets/data/organization.json')));
+          resolve(lastValueFrom(httpClient.get('/assets/data/organization.json')));
         }, 100);
       }),
     );
