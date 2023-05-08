2b01222ca8aa5d46602baef4e44cbb4b28c51f22
valeriovinciarelli <Roma?=10>
2023-05-08 18:40:44 +0200

    update the build logic and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server: '/epos-back-office/',
+  //server: '/epos-back-office/',
   apiBaseUrl: 'http://localhost:4200/api',
   useLiveApi: true,
   apiMockUrl: 'http://localhost:4200/assets/data',
