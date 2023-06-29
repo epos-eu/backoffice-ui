a27896ce7081548aa0e7b6b6ca340aedc4a93b03
Janusz Czapski <janlav@bgs.ac.uk>
2023-06-29 16:40:04 +0100

    feat: build out form for operation params, types

---
+++ src/utility/enums/operationParamsRange.enum.ts
@@ -0,0 +1,9 @@
+export enum OperationParamsRange {
+  DATE = 'xsd:date',
+  DATE_TIME = 'xsd:dateTime',
+  COORDINATE = 'xsd:coordinate',
+  BOOLEAN = 'xsd:boolean',
+  STRING = 'xsd:string',
+  INTEGER = 'xsd:integer',
+  FLOAT = 'xsd:float',
+}
