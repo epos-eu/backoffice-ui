048938fb3ef00326ac99a7e6868e03bc887bd7e7
Daniel Warren <danwar@bgs.ac.uk>
2023-07-07 16:16:03 +0100

    feat: endform hell, ensures user doesn't add param with same variable name

--- src/components/dialogs/dialog-add-new-parameter/dialog-add-new-parameter.component.ts
+++ src/components/dialogs/dialog-add-new-parameter/dialog-add-new-parameter.component.ts
@@ -4,6 +4,8 @@ import { DialogData } from '../baseDialogService.abstract';
 import { OperationParamsRange } from 'src/utility/enums/operationParamsRange.enum';
 import { Mapping } from 'src/apiAndObjects/objects/types/mapping.type';
 import { FormBuilder, FormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { OperationsService } from 'src/services/operations.service';
+import { Operation } from 'src/apiAndObjects/objects/entities/operation.model';
 
 @Component({
   selector: 'app-dialog-add-new-parameter',
@@ -13,9 +15,22 @@ import { FormBuilder, FormControl, UntypedFormGroup, Validators } from '@angular
 export class DialogAddNewParameterComponent implements OnInit {
   public ranges = Object.values(OperationParamsRange);
   public form!: UntypedFormGroup;
+  public duplicateName = false;
+  public forbiddenName = '';
+  private activeMappingArr: Array<string> = [];
   private mapping: Mapping = { range: '', variable: '', required: '' };
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData<Mapping>, private readonly formBuilder: FormBuilder) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: DialogData<Mapping>,
+    private readonly formBuilder: FormBuilder,
+    private operationService: OperationsService,
+  ) {
+    this.operationService.operationObs.subscribe((operation: Operation | null) => {
+      if (operation && operation.mapping) {
+        this.activeMappingArr = operation.mapping.map((mapping: Mapping) => mapping.variable);
+      }
+    });
+  }
 
   public ngOnInit(): void {
     this.createForm();
@@ -23,11 +38,12 @@ export class DialogAddNewParameterComponent implements OnInit {
 
   private createForm() {
     this.form = this.formBuilder.group({
-      variable: new FormControl(this.mapping.variable, Validators.required),
+      variable: new FormControl(this.mapping.variable, [Validators.required]),
       range: new FormControl(this.mapping.range, Validators.required),
       required: new FormControl(false),
     });
     this.form.valueChanges.subscribe((changes) => {
+      this.checkForSameVariableName(changes['variable']);
       this.mapping.variable = changes['variable'];
       this.mapping.range = changes['range'];
       this.mapping.required = changes['required'].toString();
@@ -42,5 +58,9 @@ export class DialogAddNewParameterComponent implements OnInit {
 
   public handleAdd(): void {
     this.data.close();
+  }
+
+  public checkForSameVariableName(value: string) {
+    this.duplicateName = this.activeMappingArr.includes(value);
   }
 }
