fed5f48a24f710eb23cabfaf332d6c68cf8efe6d
Daniel Warren <danwar@bgs.ac.uk>
2023-08-31 09:15:36 +0000

    Update environment.prod.ts var apiBaseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //server: '/epos-back-office/',
-  apiBaseUrl: 'https://ics-c.epos-ip.org/demo/k8s-epos-deploy/backoffice-test-env/api/v1',
+  apiBaseUrl: 'https://ics-c.epos-ip.org/development/k8s-epos-deploy/latest/api/v1',
   useLiveApi: true,
   apiMockUrl: 'http://localhost:4200/assets/data',
 };
