62591a157c125e99604cff9aeba805b9504e4e63
Daniel Warren <danwar@bgs.ac.uk>
2024-01-17 19:13:47 +0000

    feat: assets table always loads

--- src/components/table/table.component.ts
+++ src/components/table/table.component.ts
@@ -92,8 +92,7 @@ export class TableComponent implements AfterViewInit {
       const filters = JSON.parse(filterValue);
       return (
         data.status.trim().toLocaleLowerCase().indexOf(filters.status.trim().toLocaleLowerCase()) >= 0 &&
-        data.title?.trim().toLocaleLowerCase().indexOf(filters.title.trim().toLocaleLowerCase()) >= 0 &&
-        data.changeComment?.trim().toLocaleLowerCase().indexOf(filters.changeComment.trim().toLocaleLowerCase()) >= 0
+        data.title?.trim().toLocaleLowerCase().indexOf(filters.title.trim().toLocaleLowerCase()) >= 0
       );
     };
     this.loading = false;
