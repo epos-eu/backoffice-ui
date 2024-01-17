bd337aba23c20c004484e192de8620f91fe45110
Janusz Czapski <janlav@bgs.ac.uk>
2024-01-17 11:42:09 +0000

    chore: move new asset button

--- src/pages/browse/browse-distribution/browse-distribution.component.ts
+++ src/pages/browse/browse-distribution/browse-distribution.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/apiAndObjects/api/api.service';
+import { DataProductDetailDataSource } from 'src/apiAndObjects/objects/data-source/dataProductDetailDataSource';
+import { DataProduct } from 'src/apiAndObjects/objects/entities/dataProduct.model';
 import { EntityDetail } from 'src/apiAndObjects/objects/types/entityDetail.type';
+import { DialogNewDataproductComponent } from 'src/components/dialogs/dialog-new-dataproduct/dialog-new-dataproduct.component';
+import { DialogService } from 'src/components/dialogs/dialog.service';
+import { ActionsService } from 'src/services/actions.service';
 import { EntityService } from 'src/services/entity.service';
+import { SnackbarService } from 'src/services/snackbar.service';
 import { Entity } from 'src/utility/enums/entity.enum';
 import { EntityEndpointValue } from 'src/utility/enums/entityEndpointValue.enum';
 
@@ -13,7 +20,39 @@ import { EntityEndpointValue } from 'src/utility/enums/entityEndpointValue.enum'
 export class BrowseDistributionComponent {
   public sectionName = Entity.DISTRIBUTION;
 
-  constructor(private router: Router, private entityService: EntityService) {}
+  constructor(
+    private router: Router,
+    private entityService: EntityService,
+    private dialogService: DialogService,
+    private apiService: ApiService,
+    private snackbarService: SnackbarService,
+    private actionsService: ActionsService,
+  ) {}
+
+  private handleCreate(): void {
+    const item: DataProduct = {
+      created: new Date(),
+    };
+
+    this.apiService.endpoints.DataProduct.create
+      .call(item)
+      .then((value: DataProductDetailDataSource) => {
+        this.router.navigate([`/browse/${EntityEndpointValue.DATA_PRODUCT}/details`, value.metaId, value.instanceId]);
+        this.snackbarService.openSnackbar(`Success: ${value.uid} created`, 'close', 'success', 6000, [
+          'snackbar',
+          'mat-toolbar',
+          'snackbar-success',
+        ]);
+        this.actionsService.saveCurrentEdit(value.instanceId);
+      })
+      .catch(() =>
+        this.snackbarService.openSnackbar(`Error: failed to create new Data Product`, 'close', 'error', 6000, [
+          'snackbar',
+          'mat-toolbar',
+          'snackbar-error',
+        ]),
+      );
+  }
 
   public rowClicked(row: Record<string, unknown>): void {
     const dataProduct = row['dataProduct'] as EntityDetail;
@@ -27,7 +66,13 @@ export class BrowseDistributionComponent {
     }
   }
 
-  public createDistribution(): void {
-    this.router.navigate([`browse/${EntityEndpointValue.DISTRIBUTION}/new`]);
+  public createAsset(): void {
+    this.dialogService
+      .openDialogForComponent(DialogNewDataproductComponent, {}, 'new-dataproduct-dialog')
+      .then((response) => {
+        if (response.dataOut.create) {
+          this.handleCreate();
+        }
+      });
   }
 }
