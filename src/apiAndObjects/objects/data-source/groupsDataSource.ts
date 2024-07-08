145b6a266b93e277a36a46009d17bf44f5fef24b
Daniel <danwar@bgs.ac.uk>
2024-07-08 13:36:47 +0100

    feat: group API implementation

--- src/apiAndObjects/objects/data-source/groupsDataSource.ts
+++ src/apiAndObjects/objects/data-source/groupsDataSource.ts
@@ -4,15 +4,24 @@ export class GroupsDataSource extends BaseObject {
   public static readonly KEYS = {
     NAME: 'name',
     DESCRIPTION: 'description',
+    ENTITIES: 'entities',
+    ID: 'id',
+    USERS: 'users',
   };
 
   public readonly name: string;
   public readonly description: string;
+  public readonly entities: Array<string>;
+  public readonly id: string;
+  public readonly users: Array<string>;
 
   protected constructor() {
     super();
 
     this.name = this._getString(GroupsDataSource.KEYS.NAME);
     this.description = this._getString(GroupsDataSource.KEYS.DESCRIPTION);
+    this.entities = this._getArray(GroupsDataSource.KEYS.ENTITIES);
+    this.id = this._getString(GroupsDataSource.KEYS.ID);
+    this.users = this._getArray(GroupsDataSource.KEYS.USERS);
   }
 }
