7af9ee350da8198ca60831860d0c52381d1a091e
Daniel <danwar@bgs.ac.uk>
2024-08-13 15:01:41 +0100

    feat: removed old leaflet fullscreen plugin and installed new maintained one with types

--- src/pages/browse/browse-data-products/browse-data-products-item/spatial-coverage/spatial-coverage-map/spatial-coverage-map.component.ts
+++ src/pages/browse/browse-data-products/browse-data-products-item/spatial-coverage/spatial-coverage-map/spatial-coverage-map.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet.fullscreen';
 import { Subject } from 'rxjs';
 import { SpatialCoverageType } from 'src/utility/enums/spatialCoverageType.enum';
 
@@ -50,6 +51,11 @@ export class SpatialCoverageMapComponent implements AfterViewInit, OnInit {
       boxZoom: false,
       keyboard: false,
       dragging: true,
+      fullscreenControl: true,
+      fullscreenControlOptions: {
+        position: 'topleft',
+        content: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-120v-200h80v120h120v80H120Zm520 0v-80h120v-120h80v200H640ZM120-640v-200h200v80H200v120h-80Zm640 0v-120H640v-80h200v200h-80Z"/></svg>`,
+      },
     });
     tiles.addTo(this.map);
     this.getDataOnMaps();
