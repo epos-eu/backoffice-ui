3779adfa6d9b6d0fc2f486e7e796a228a823e18c
Valerio Vinciarelli <valerio.vinciarelli@epos-eric.eu>
2024-12-02 14:21:33 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,9 @@ import { LogLevel } from 'src/utility/enums/log.enum';
 
 export const environment = {
   production: true,
+  server: 'http://gateway:5000',
   //server: '/epos-back-office/',
-  apiBaseUrl: 'https://ics-c.epos-ip.org/development/k8s-epos-deploy/latest/api/v1',
+  apiBaseUrl: 'http://gateway:5000/api/v1',
   useLiveApi: true,
   apiMockUrl: 'http://localhost:4200/assets/data',
   logLevel: LogLevel.info,
