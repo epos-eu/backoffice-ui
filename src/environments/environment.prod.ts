4a826c6b2dce6f48393e6a2ff426328cb033170f
Mario Malitesta <mario.malitesta@epos-eric.eu>
2025-06-13 12:51:46 +0200

    change api from /refactoring to /latest

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { LogLevel } from 'src/utility/enums/log.enum';
 export const environment = {
   production: true,
   //server: '/epos-back-office/',
-  apiBaseUrl: 'https://ics-c.epos-ip.org/development/k8s-epos-deploy/refactoring/api/v1',
+  apiBaseUrl: 'https://ics-c.epos-ip.org/development/k8s-epos-deploy/latest/api/v1',
   useLiveApi: true,
   apiMockUrl: 'http://localhost:4200/assets/data',
   logLevel: LogLevel.info,
