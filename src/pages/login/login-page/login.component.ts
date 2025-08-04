d5a0b3e91f1b8887e66014f9a1adfdafeddadbf3
Mario Malitesta <mario.malitesta@epos-eric.eu>
2025-08-04 16:09:54 +0200

    remove EPOS references (logos, texts...) from ENVRI Backoffice

--- src/pages/login/login-page/login.component.ts
+++ src/pages/login/login-page/login.component.ts
@@ -12,7 +12,7 @@ import { UserBackofficeInfo } from 'src/utility/objects/userBackofficeInfo';
 export class LoginPageComponent implements OnInit {
   constructor(private aaaiService: AaaiService, private activeUserService: ActiveUserService) {}
 
-  public imgUrl = 'assets/img/logo.svg';
+  public imgUrl = 'assets/img/ENVRI-Hub-NEXT-logo.png';
   public manageUrl!: string;
   public user: null | AAAIUser = null;
   public userInfo: UserBackofficeInfo | null = null;
