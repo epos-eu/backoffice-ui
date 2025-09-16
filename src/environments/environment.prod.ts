3fe46d7000d035251d9de7d196318b2a5f21fd64
Mario Malitesta <mario.malitesta@epos-eric.eu>
2025-09-16 17:08:51 +0200

    still TEST: change base api url Live

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { LogLevel } from 'src/utility/enums/log.enum';
 export const environment = {
   production: true,
   //server: '/epos-back-office/',
-  apiBaseUrl: window.location.href.includes('/backoffice/home')?window.location.href.replace('/backoffice/home', '')+'/api/v1' : window.location.href+'/api/v1',
+  apiBaseUrl: 'https://catalogue.staging.envri.eu/api/v1',
   useLiveApi: true,
   apiMockUrl: 'http://localhost:4200/assets/data',
   logLevel: LogLevel.info,
